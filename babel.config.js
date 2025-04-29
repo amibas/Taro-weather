97f9fcd3854a6114dd4900e89ea07416b3599945
amiba <towsod@outlook.com>
2025-04-29 22:34:06 +0800

    build(babel): 注释掉 babel 插件配置
    - 注释掉了 @taroify/core 和 @taroify/icons 的 import 插件配置
    - 这可能会暂时影响到这两个库的按需加载功能

--- babel.config.js
+++ babel.config.js
@@ -2,28 +2,28 @@
 // https://docs.taro.zone/docs/next/babel-config
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-  plugins: [
-    [
-      "import",
-      {
-        libraryName: "@taroify/core",
-        libraryDirectory: "",
-        style: true,
-      },
-      "@taroify/core",
-    ],
-    [
-      "import",
-      {
-        libraryName: "@taroify/icons",
-        libraryDirectory: "",
-        camel2DashComponentName: false,
-        style: () => "@taroify/icons/style",
-        customName: (name) => name === "Icon" ? "@taroify/icons/van/VanIcon" : `@taroify/icons/${name}`,
-      },
-      "@taroify/icons",
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     "import",
+  //     {
+  //       libraryName: "@taroify/core",
+  //       libraryDirectory: "",
+  //       style: true,
+  //     },
+  //     "@taroify/core",
+  //   ],
+  //   [
+  //     "import",
+  //     {
+  //       libraryName: "@taroify/icons",
+  //       libraryDirectory: "",
+  //       camel2DashComponentName: false,
+  //       style: () => "@taroify/icons/style",
+  //       customName: (name) => name === "Icon" ? "@taroify/icons/van/VanIcon" : `@taroify/icons/${name}`,
+  //     },
+  //     "@taroify/icons",
+  //   ],
+  // ],
   presets: [
     ['taro', {
       framework: 'react',

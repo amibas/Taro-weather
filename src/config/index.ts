c579ff46aafd476ce15dc48eb34007883bcd9aca
amiba <towsod@outlook.com>
2025-05-01 00:59:48 +0800

    feat(config): 添加高德地图地理编码相关 URL
    - 在 amapConfig 中添加 geoUrl，用于地理编码查询
    - 将原有的 regeoUrl 保留，用于反向地理编码

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 export const amapConfig = {
   weatherUrl: 'https://restapi.amap.com/v3/weather/weatherInfo',
-  geoUrl: 'https://restapi.amap.com/v3/geocode/regeo',
+  regeoUrl: 'https://restapi.amap.com/v3/geocode/regeo',
+  geoUrl: 'https://restapi.amap.com/v3/geocode/geo',
   key: '1cd4e72fafde9ece8132573112bff72e'
 };
